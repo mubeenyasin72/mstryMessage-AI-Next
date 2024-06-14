e59b337b7934e0b517fb3cdafa213e022f30317c
mubeenyasin72 <mubeenyasin75@gmail.com>
2024-06-14 17:59:38 +0500

    working on next auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,38 @@
+import { NextAuthOptions } from "next-auth";
+import CreadentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnection from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CreadentialsProvider({
+      id: "credentials",
+      name: "Creadentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnection();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.password },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account first before login");
+          }
+            const hashPassword = await bcrypt.compare(credentials.password)
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+};
